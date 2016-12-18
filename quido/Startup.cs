fe160e31c578464ed975eb4a4f4291bf55223111
Michal <falat.mich@gmail.com>
2016-12-18 14:30:39 +0100

    Add project files.

---
+++ quido/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(quido.Startup))]
+namespace quido
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
